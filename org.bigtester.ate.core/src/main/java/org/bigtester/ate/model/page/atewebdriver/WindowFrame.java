7ef4deb5b6613fe6c8adb163062bf1f9c28435f9
peidong <zhouyibhic@gmail.com>
2018-04-08 04:17:58 +0000

    fix of fraime refresh

--- org.bigtester.ate.core/src/main/java/org/bigtester/ate/model/page/atewebdriver/WindowFrame.java
+++ org.bigtester.ate.core/src/main/java/org/bigtester/ate/model/page/atewebdriver/WindowFrame.java
@@ -106,7 +106,7 @@ public class WindowFrame {
 			myWd.switchTo().frame(this.getFrame());
 		} catch (WebDriverException thr) {
 			String msg = thr.getMessage();
-			if (null!=msg && (msg.contains("is not attached") || msg.contains("is no longer attached"))) { 
+			if (null!=msg && (msg.contains("is not attached") || msg.contains("is no longer attached") || msg.contains("Unable to locate frame"))) { 
 				throw new PageFrameRefreshException(msg, ExceptionErrorCode.WINDOWFRAME_REFRESH, this);//NOPMD
 			} else {
 				throw GlobalUtils.createInternalError("obtainFrameFocus", thr);
